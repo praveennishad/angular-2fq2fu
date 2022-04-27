4ecc28efa738ab02dd4ae06b08a5521eb58bd92c
praveennishad <pnishad03@gmail.com>
2022-04-27 18:38:47 +0530

    1 file modified

--- src/app/formelement/formelement.component.ts
+++ src/app/formelement/formelement.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'my-formelement',
   templateUrl: './formelement.component.html',
 })
-export class FormelementComponent {
+export class FormelementComponent implements OnChanges {
   onClickSubmit(data) {
     alert('Entered Email id : ' + data.emailid);
+  }
+  ngOnChanges() {
+    console.log('OnInit Life Cycle Hook');
   }
 }
